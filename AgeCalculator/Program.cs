8026e97484fdaa0a91c31e9609681463843e7bfe
Corey Hughes <Hughes-C109@ulster.ac.uk>
2024-09-24 15:34:19 +0100

    Final Commit

--- AgeCalculator/Program.cs
+++ AgeCalculator/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+Console.WriteLine("Please enter your First Name: ");
+string firstName = Console.ReadLine();
+
+
+Console.WriteLine("Please enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+int finalAge;
+finalAge = age + 5;
+
+Console.WriteLine("Hello {0}. You will be {1} in 5 years!", firstName, finalAge);
+
